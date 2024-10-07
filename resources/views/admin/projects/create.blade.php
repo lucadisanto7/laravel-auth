11b5c2290b28bf53cea17860e2e89300346796a8
lucadisanto7 <lucadisanto47@gmail.com>
2024-10-07 19:28:10 +0200

    [UPD] added textarea

---
+++ resources/views/admin/projects/create.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.dashboards')
+
+@section('main-content')
+    <div class="container-full">
+        <div class="row">
+            <div class="col-12">
+                <h2>Aggiungi un nuovo progetto</h2>
+            </div>
+            <div class="col-12">
+                
+                <form action="{{ route('admin.projects.store')}}" method="POST">
+                    @csrf
+                    <div class="row">
+                        <div class="col-12">
+                            <label for="" class="control-label">Nome Progetto</label>
+                            <input type="text" name="name" id="" class="form-control form-control-sm" placeholder="Nome Progetto"> 
+                        </div>
+                        <div class="col-12">
+                            <textarea name="summary" id="" cols="30" rows="10" class="form-control form-control-sm"></textarea>
+                        </div>
+                        <div class="col-12">
+                            <label for="" class="control-label">Nome Progetto</label>
+                            <input type="text" name="name" id="" class="form-control form-control-sm" placeholder="Nome Progetto"> 
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+@endsection
