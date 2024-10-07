79215747c2d53bbb3e071169e5e57203f728b961
lucadisanto7 <lucadisanto47@gmail.com>
2024-10-07 19:09:47 +0200

    [UPD] Added table

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -18,6 +18,22 @@
                             <th>Strumenti</th>
                         </tr>
                     </thead>
+                    <tbody>
+                        @foreach ($projects as $project)
+                            <tr>
+                                <td>{{ $project->id }}</td>
+                                <td>{{ $project->name }}</td>
+                                <td>{{ $project->slug }}</td>
+                                <td>
+                                    <div class="d-flex">
+                                        <a href="" class="btn btn-sm btn-primary">
+                                            <i class="fas fa-eye"></i>
+                                        </a>
+                                    </div>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
                 </div>
             </div>
         </div>
