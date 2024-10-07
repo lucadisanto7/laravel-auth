468f60c8c149b83024a395180eb3e1cf84d67db4
lucadisanto7 <lucadisanto47@gmail.com>
2024-10-07 19:50:46 +0200

    [FIX] Fixed alert

--- resources/views/admin/projects/create.blade.php
+++ resources/views/admin/projects/create.blade.php
@@ -8,7 +8,7 @@
             </div>
             <div class="col-12">
                 @if ($errors->any())
-                    <div class="aler alert-danger">
+                    <div class="alert alert-danger">
                         <ul class="list-unstyled">
                             @foreach ($errors->all() as $error)
                                 <li>{{$error}} </li>
