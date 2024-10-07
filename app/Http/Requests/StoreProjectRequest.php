e69cde2e983394a71570306e8c75c5809e2542da
lucadisanto7 <lucadisanto47@gmail.com>
2024-10-07 19:45:13 +0200

    [UPD] Added store project

---
+++ app/Http/Requests/StoreProjectRequest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreProjectRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|max:200',
+            'slug' => 'required|max:255'
+
+        ];
+    }
+
+        public function messages(){
+            return[
+                "name.required" => "il nome e' obbigatorio",
+                "name.max" => "il nome del progetto deve essere lungo al massimo :max caratteri",
+                "slug.required" => "Lo slug e' obbigatorio",
+                "slug.max" => "Lo slug del progetto deve essere lungo al massimo :max caratteri",
+            ];
+        }
+}
