8cb98d4973c349619ccfa1ad4f535c0605b7636e
lucadisanto7 <lucadisanto47@gmail.com>
2024-10-09 17:39:35 +0200

    [UPD] Added js

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,3 +5,16 @@ import * as bootstrap from 'bootstrap';
 import.meta.glob([
     '../img/**'
 ])
+
+const dleteButtons = document.querySelectorAll('delete-buttons');
+deleteButtons.forEach((button) =>{
+    button.addEventListener('click', function(event){
+        event.preventDefault();
+        const modal = document.getElementById('deleteProjectModal');
+        const bootstrapModal = new bootstrap.Modal(modal);
+        bootstrapModal.show();
+        document.querySelector('.confirm-delete').addEventListener('click', function(){
+            button.parentElement.submit();
+        })
+    });
+});
