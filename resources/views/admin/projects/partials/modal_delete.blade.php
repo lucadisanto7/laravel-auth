e5834448cee3f50404c747f44aad8349d7309c1d
lucadisanto7 <lucadisanto47@gmail.com>
2024-10-09 17:34:58 +0200

    [UPD] Added text

--- resources/views/admin/projects/partials/modal_delete.blade.php
+++ resources/views/admin/projects/partials/modal_delete.blade.php
@@ -2,15 +2,15 @@
     <div class="modal-dialog">
       <div class="modal-content">
         <div class="modal-header">
-          <h1 class="modal-title fs-5" id="deleteProjectModal">Modal title</h1>
+          <h1 class="modal-title fs-5" id="deleteProjectModal">Conferma cancellazione</h1>
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
         <div class="modal-body">
-          ...
+          Sei sicuro di voler cancellare?
         </div>
         <div class="modal-footer">
           <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-          <button type="button" class="btn btn-primary">Save changes</button>
+          <button type="button" class="btn btn-primary">Cancel</button>
         </div>
       </div>
     </div>
