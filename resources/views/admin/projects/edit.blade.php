c5d38374355282601f39b060e741606dfd515a78
lucadisanto7 <lucadisanto47@gmail.com>
2024-10-09 17:12:55 +0200

    [UPD] Added list

--- resources/views/admin/projects/edit.blade.php
+++ resources/views/admin/projects/edit.blade.php
@@ -6,7 +6,9 @@
             <div class="col-12">
                 <div class="alert-alert-danger">
                     <ul class="list-unstyled">
-
+                        @foreach ($errors->all() as $error)
+                                <li>{{$error}} </li>
+                            @endforeach
                     </ul>
                 </div>
                 <form action="{{ route('admin.projects.update', ['project' => $project->id])}}" method="POST">
@@ -16,12 +18,12 @@
                     <div class="col-12">
                         <label for="" class="control-label">Nome progetto</label>
                         <input type="text" name="name" id="" class="form-control form-control-sm" 
-                            placeholder="Nome progetto" value="{{ old('name')}}">
+                            placeholder="Nome progetto" value="{{ old('name', $project->name)}}">
                     </div>
                     <div class="col-12">
                         <label for="" class="control-label">Sommario progetto</label>
                         <input type="text" name="name" id="" class="form-control form-control-sm" 
-                            placeholder="Nome progetto" value="{{ old('summary')}}">
+                            placeholder="Nome progetto" value="{{ old('summary', $project->summary)}}">
                     </div>         
                     <div class="col-12">
                             <button type="submit" class="btn btn-sm btn-success">Salva</button>
