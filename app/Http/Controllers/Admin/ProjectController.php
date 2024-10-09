e3899b1ba7d3e428436165046837f0fb6257dc2c
lucadisanto7 <lucadisanto47@gmail.com>
2024-10-09 17:24:35 +0200

    [UPD] Added destroy

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -93,6 +93,7 @@ class ProjectController extends Controller
      */
     public function destroy(Project $project)
     {
-        //
+        $project->delete();
+        return redirect()->route('admin.project.index');
     }
 }
