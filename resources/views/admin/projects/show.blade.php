d06d86e983b57c92c9122e83f89cde49bdb34979
lucadisanto7 <lucadisanto47@gmail.com>
2024-10-11 10:05:04 +0200

    [UPD] Added placeholder img to show.php

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -5,6 +5,7 @@
     <div class="row">
         <div class="col-12">
             <h2>{{$project->name}}</h2>
+            <img class="project-image" src="{{ $project->image !== null ? asset('./storage/'.$project->image) : 'https://plachehold.co/600x400?text=Immagine+copertina'}}" alt="{{ $project->name}}">
             <p>{{$project->slug}}</p>
             <p>{{$project->summary}}</p>
         </div>
